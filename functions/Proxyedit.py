eff71dd09edcd42df4109732622a957d5a58d17c
postmanoy <alfonso_munez@trendmicro.com>
2021-01-13 07:32:07 +0800

    added web rep and recon scan ip/port list

--- functions/Proxyedit.py
+++ functions/Proxyedit.py
@@ -151,6 +151,26 @@ def ProxyEdit(allofpolicy, t1iplistid, t2iplistid, t1portlistid, t2portlistid):
                                     indexnum = t1iplistid.index(indexid1)
                                     listpart = indexid5.replace(indexid1, t2iplistid[indexnum])
                                     describe = describe.replace(indexid5, listpart)
-        
+        index = describe.find('webReputationSettingMonitorPortListId')
+        if index != -1:
+            indexpart = describe[index:]
+            startIndex = indexpart.find('{')
+            if startIndex != -1: #i.e. if the first quote was found
+                endIndex = indexpart.find('}', startIndex + 1)
+                if startIndex != -1 and endIndex != -1: #i.e. both quotes were found
+                    indexid = indexpart[startIndex+1:endIndex]
+                    value = indexid.find("value")
+                    if value != -1:
+                        valuepart = indexid[value+7:]
+                        startvalue = valuepart.find('\"')
+                        if startvalue != -1:
+                            endvalue = valuepart.find('\"', startvalue+1)
+                            if startvalue != -1 and endvalue != -1:
+                                indexid1 = valuepart[startvalue+1:endvalue]
+                                if indexid1 != "" and indexid1 != '80,8080':
+                                    indexid5 = describe[index:index+endIndex]
+                                    indexnum = t1portlistid.index(indexid1)
+                                    listpart = indexid5.replace(indexid1, t2portlistid[indexnum])
+                                    describe = describe.replace(indexid5, listpart)
         allofpolicy[count] = describe
     return allofpolicy
